26ec9bf04df9a42ec09890b60feb0bb1ea71c3e0
Dion <ksundong@gmail.com>
2020-11-24 22:57:34 +0900

    feat: 라우트 테이블에 라우트 블럭을 사용해서 설정

--- chapter8/vpc.tf
+++ chapter8/vpc.tf
@@ -37,6 +37,11 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.vpc-dion.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
   tags = {
     "Name" = "terraform101-rt-public"
   }
