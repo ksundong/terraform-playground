750cbe86bbdcb00598e224789ba59ff5f22de7fb
Dion <ksundong@gmail.com>
2020-11-26 00:19:35 +0900

    feat: IAM Group Policy 설정

--- iam/devops_group.tf
+++ iam/devops_group.tf
@@ -11,3 +11,26 @@ resource "aws_iam_group_membership" "devops" {
 
   group = aws_iam_group.devops_group.name
 }
+
+resource "aws_iam_group_policy" "devops_group_policy" {
+  name = "devops_group"
+  group = aws_iam_group.devops_group.name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "*"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+
+}
