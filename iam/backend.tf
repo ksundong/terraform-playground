a2e5c7f5e2d5657b402736b29bcc13ccf009a298
Dion <ksundong@gmail.com>
2020-11-26 21:32:13 +0900

    feat: iam terraform project에 backend 설정 후 tfstate 업로드

---
+++ iam/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "tf101-dion-apne2-tfstate"
+    key = "terraform-playground/iam/terraform.tfstate"
+    region = "ap-northeast-2"
+    encrypt = true
+    dynamodb_table = "terraform-lock"
+  }
+}
